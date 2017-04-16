38610fe667d6ed638546a32108ff7fcb863b98d6
Your Name <ermolaeva@agent.ru>
2017-04-16 22:28:43 +1000

    eliminate depricated method

--- url_test.py
+++ url_test.py
@@ -6,12 +6,12 @@ from url import get_url
 class UrlCreationTestCase(unittest.TestCase):
     def test_get_url_no_param(self):
         arguments = {'--help': False, '--page': None, '<ticket>': None, 'show': True}
-        self.assertEquals(get_url(arguments), 'http://127.0.0.1:3000/tickets')
+        self.assertEqual(get_url(arguments), 'http://127.0.0.1:3000/tickets')
 
     def test_get_url_ticket(self):
         arguments = {'--help': False, '--page': None, '<ticket>': '0', 'show': True}
-        self.assertEquals(get_url(arguments), "http://127.0.0.1:3000/tickets/0")
+        self.assertEqual(get_url(arguments), "http://127.0.0.1:3000/tickets/0")
 
     def test_get_url_page(self):
         arguments = {'--help': False, '--page': 1, '<ticket>': None, 'show': True}
-        self.assertEquals(get_url(arguments), ('http://127.0.0.1:3000/tickets', {'page': '1'}))
+        self.assertEqual(get_url(arguments), ("http://127.0.0.1:3000/tickets", {'page': '1'}))
