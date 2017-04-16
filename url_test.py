429a0755bdb214476bbd9e6f02fa6d6bcc41c4f9
Your Name <ermolaeva@agent.ru>
2017-04-16 16:21:56 +1000

    added url building test

---
+++ url_test.py
@@ -0,0 +1,17 @@
+import unittest
+
+from url import get_url
+
+
+class DefaultWidgetSizeTestCase(unittest.TestCase):
+    def test_get_url_no_param(self):
+        arguments = {'--help': False, '--page': None, '<ticket>': None, 'show': True}
+        self.assertEquals(get_url(arguments), 'http://127.0.0.1:3000/tickets')
+
+    def test_get_url_ticket(self):
+        arguments = {'--help': False, '--page': None, '<ticket>': '0', 'show': True}
+        self.assertEquals(get_url(arguments), "http://127.0.0.1:3000/tickets/0")
+
+    def test_get_url_page(self):
+        arguments = {'--help': False, '--page': 1, '<ticket>': None, 'show': True}
+        self.assertEquals(get_url(arguments), ('http://127.0.0.1:3000/tickets', {'page': '1'}))
