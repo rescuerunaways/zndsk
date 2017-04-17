e966bdefa19f828976894e6ccaf3f015dfa57a3a
Your Name <ermolaeva@agent.ru>
2017-04-17 20:40:57 +1000

    refactored controller, added error handler

---
+++ controller.py
@@ -0,0 +1,12 @@
+from json import dumps
+
+import requests
+
+from url import get_url
+
+
+def get(args):
+    res = requests.get(*get_url(args))
+    if not res.ok:
+        return dumps("Server error:{0}".format(Exception(res.reason, res.status_code)))
+    return res
